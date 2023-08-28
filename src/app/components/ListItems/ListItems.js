cf72a0305a33388bd7c1bd4f31bb577267bf2c87
pengsterism <zahira.davies@mecca.com.au>
2023-08-28 16:17:39 +1000

    - added edit and remove - added styled-components - removed css

--- src/app/components/ListItems/ListItems.js
+++ src/app/components/ListItems/ListItems.js
@@ -1,13 +1,82 @@
-import React from 'react';
+"use client"
 
-function ListItems({ tasks }) {
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+
+
+const colors = ['#ECBAA8', '#CC1426', '#56223D', '#EF60A3', '#000000'];
+
+const StyledUl = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  align-items:center;
+  justify-content: center;
+`;
+
+const StyledLi = styled.li`
+  margin-bottom: 8px;
+  background-color: ${props => props.color || 'white'};
+  padding: 10px;
+  border-radius: 5px;
+`;
+
+const Button = styled.button`
+  padding: 5px 10px;
+  border: none;
+  background-color: #ddd;
+  cursor: pointer;
+  border-radius: 3px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const ButtonContainer = styled.div`
+justify-content: right;
+align-items: right;
+display: flex;
+column-gap: 5px;
+`;
+
+function ListItems({ tasks, handleEdit, handleDelete, editingIndex, handleUpdate }) {
+
+  const [editedTask, setEditedTask] = useState('');
+
+  const handleChange = event => {
+    setEditedTask(event.target.value);
+  };
+
+  const handleSave = index => {
+    handleUpdate(index, editedTask);
+    setEditedTask('');
+    handleEdit(-1);
+  };
   return (
-    <ul>
-      {tasks.map((task, index) => (
-        <li key={index}>{task}</li>
-      ))}
-    </ul>
-  );
+    <StyledUl>
+    {tasks.map((task, index) => (
+      <StyledLi key={index} color={colors[index % colors.length]}>
+        {editingIndex === index ? (
+          <>
+            <input type="text" value={editedTask} onChange={handleChange} />
+            <div>
+              <Button onClick={() => handleSave(index)}>Save</Button>
+              <Button onClick={() => handleDelete(index)}>Delete</Button>
+            </div>
+          </>
+        ) : (
+          <>
+            {task}
+            <ButtonContainer>
+              <Button onClick={() => handleEdit(index)}>Edit</Button>
+              <Button onClick={() => handleDelete(index)}>Delete</Button>
+            </ButtonContainer>
+          </>
+        )}
+      </StyledLi>
+    ))}
+  </StyledUl>
+);
 }
+
 
 export default ListItems;
--- src/app/components/TodoList/TodoList.js
+++ src/app/components/TodoList/TodoList.js
@@ -4,18 +4,42 @@ import React, { useState } from 'react';
 import InputForm from '../InputForm/InputForm';
 import ListItems from '../ListItems/ListItems';
 
+
+
 function TodoList() {
   const [tasks, setTasks] = useState([]);
+  const [editingIndex, setEditingIndex] = useState(-1);
 
   const addTask = (task) => {
     setTasks([...tasks, task]);
+  };
+
+  const handleEdit = (index) => {
+    setEditingIndex(index);
+  };
+
+  const handleDelete = (index) => {
+    const newTasks = tasks.filter((_, i) => i !== index);
+    setTasks(newTasks);
+  };
+
+  const handleUpdate = (index, updatedTask) => {
+    const updatedTasks = tasks.map((task, i) => (i === index ? updatedTask : task));
+    setTasks(updatedTasks);
+    setEditingIndex(-1);
   };
 
   return (
     <div>
       <h1>To-Do List</h1>
       <InputForm addTask={addTask} />
-      <ListItems tasks={tasks} />
+      <ListItems
+        tasks={tasks}
+        handleEdit={handleEdit}
+        handleDelete={handleDelete}
+        editingIndex={editingIndex}
+        handleUpdate={handleUpdate}
+      />
     </div>
   );
 }
