409fbc2131cd7d8878839d59873de8fdab44d331
pengsterism <zahira.davies@mecca.com.au>
2023-09-01 14:38:20 +1000

    added routing

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,26 @@
-import React from 'react';
-import TodoList from './components/TodoList/TodoList';
+"use client"
 
-export default function Home() {
+import React from 'react';
+import Link from 'next/link';
+import styled from "styled-components";
+
+const Homepage = styled.div`
+  list-style-type: none;
+  padding: 0;
+  align-items: center;
+  justify-content: center;
+  font-family: Euclid Circular B, sans-serif;
+  flex-direction: column;
+`;
+
+function Home() {
   return (
-    <div>
-      <TodoList />
-    </div>
+    <Homepage>
+      <h1>Zahira's projects!</h1>
+      <p>Click the button below to go to the to-do list page:</p>
+      <Link href="/todo-list">Go to To-Do List</Link>
+    </Homepage>
   );
 }
+
+export default Home;
